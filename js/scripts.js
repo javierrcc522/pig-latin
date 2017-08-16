15ee1b5309bbc1acb66df918b92bfaef0f9194e5
javier jimenez and senya webster <javier@>
2017-08-16 16:53:34 -0700

    add final edit

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,21 @@ var consonants = ["b", "B", "c", "C", "d", "D", "f", "F", "g", "G", "h", "H", "j
 // this is a for loop starting at 0, rolling through the vowels one a time
 // if loop meets criteria it concatenates "way" onto the back
 
-function wordSplitter(str) {
+// function wordSplitter(str) {
   //str[index] = userPigLatin[index];
 
   //alert(str); // [word,word1,word2
-  return str;
+  // return str;
   // for(var index = 0; index < str.length; index ++) {
   //   if (str.split(""));
   //   //alert(str.join());
   //   return ;
   // };
-};
+// };
 
 function userPigLatin(str) {
   str = str.split(" ");
-  for(var j = 0; j < str.length; j++){
+  for(var j = 0; j < str.length; j ++){
     for(var index = 0; index < vowels.length; index ++) {
       if (str[j].charAt(0) === vowels[index]) {
         str[j] = str[j].concat("way");
@@ -36,7 +36,9 @@ function userPigLatin(str) {
   return str.join(" ");
 };
 
+function cons(str) {
 
+}
 
 
 
